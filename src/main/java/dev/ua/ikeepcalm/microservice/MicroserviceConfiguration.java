e784fd6d426faa629d086d9a4ce908abc25e3c5d
Horokh Bohdan <horokhbohdandmytrovich@gmail.com>
2024-09-17 17:28:54 +0300

    Disable CORS

---
+++ src/main/java/dev/ua/ikeepcalm/microservice/MicroserviceConfiguration.java
@@ -0,0 +1,16 @@
+package dev.ua.ikeepcalm.microservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MicroserviceConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
